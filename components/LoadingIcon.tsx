792d664b37f8a40f6ef484dcde04fb4cc3129d19
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-10-25 14:03:19 +0530

    Add LoadingIcon component

---
+++ components/LoadingIcon.tsx
@@ -0,0 +1,7 @@
+export default function LoadingIcon() {
+  return (
+    <div className="inset-0 flex items-center justify-center">
+      <div className="h-6 w-6 animate-spin rounded-full border-2 border-white border-t-transparent" />
+    </div>
+  );
+}
